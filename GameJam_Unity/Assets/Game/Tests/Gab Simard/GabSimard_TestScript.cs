ad6f38d90a52cb289a84a260a681008303204c0e
PC-GABRIEL\Utilisateur <gab_sim16@hotmail.com>
2017-10-06 15:50:16 -0400

    sup les bitch test scrip

--- GameJam_Unity/Assets/Game/Tests/Gab Simard/GabSimard_TestScript.cs
+++ GameJam_Unity/Assets/Game/Tests/Gab Simard/GabSimard_TestScript.cs
@@ -6,11 +6,11 @@ public class GabSimard_TestScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        print("sup les bitches");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
 }
