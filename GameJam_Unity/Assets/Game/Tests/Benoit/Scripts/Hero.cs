dc6687bd8d03e077e3e7d3bc0560258201afc938
Benoit <fivocx@gmail.com>
2017-10-06 20:31:31 -0400

    Snap des hero fictif sur des node

---
+++ GameJam_Unity/Assets/Game/Tests/Benoit/Scripts/Hero.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+
+
+    // Use this for initialization
+    public void SnapToNode()
+    {
+        print("asd");
+        float minSqrDistance = float.PositiveInfinity;
+        nodesTest closestNode = null;
+
+        nodesTest[] nodes = FindObjectsOfType(typeof(nodesTest)) as nodesTest[];
+        foreach (nodesTest node in nodes)
+        {
+            float sqrDistance = (node.transform.position - transform.position).sqrMagnitude;
+            if (sqrDistance < minSqrDistance)
+            {
+                minSqrDistance = sqrDistance;
+                closestNode = node;
+            }
+        }
+        if (closestNode != null)
+            transform.position = closestNode.transform.position;
+        else
+            Destroy(gameObject);
+    }
+}
