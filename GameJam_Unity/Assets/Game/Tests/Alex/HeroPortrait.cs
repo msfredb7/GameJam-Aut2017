ecc9feeb5bc58fc59383054d90c0744dacf71fe6
Xeal <alexandre-blanchet94@hotmail.com>
2017-10-07 01:37:59 -0400

    Changement au UI en général

---
+++ GameJam_Unity/Assets/Game/Tests/Alex/HeroPortrait.cs
@@ -0,0 +1,12 @@
+﻿using CCC.Manager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroPortrait : MonoBehaviour {
+
+	public void OnPortraitClicked()
+    {
+        Scenes.LoadAsync(DisplayBehavior.SCENE_NAME,UnityEngine.SceneManagement.LoadSceneMode.Additive);
+    }
+}
