0da39f7907fff506b87a53a8453f8304b2a9bc1d
Xeal <alexandre-blanchet94@hotmail.com>
2017-10-08 13:39:29 -0400

    fix zavier

--- GameJam_Unity/Assets/Game/Tests/Alex/Actions/DeployAction.cs
+++ GameJam_Unity/Assets/Game/Tests/Alex/Actions/DeployAction.cs
@@ -2,11 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class DeployAction : HeroActionEvent
 {
     DeployActionInfo deployActionInfo;
     private Action myOnComplete;
+    private ZavierZone zone;
+    private Hero hero;
+
+    private Node currentOrderNode;
+    private Node currentPizzaNode;
+    private bool goingToPizza;
+    private bool goingToOrder;
 
     public DeployAction()
     {
@@ -28,24 +36,104 @@ public class DeployAction : HeroActionEvent
     public override void Execute(Hero hero, Action onComplete)
     {
         // deploy
-        ZavierZone zavierzone = hero.GetComponent<ZavierZone>();
+        this.hero = hero;
+        zone = hero.GetComponent<ZavierZone>();
 
-        if (zavierzone != null)
+        if (zone != null)
         {
-            zavierzone.zonePreview.enabled = true;
+            zone.zonePreview.DOFade(1, 0.5f);
             myOnComplete = delegate ()
             {
-                zavierzone.zonePreview.enabled = false;
+                zone.zonePreview.DOFade(0, 0.5f);
                 onComplete.Invoke();
             };
 
-            zavierzone.onZoneClear += () =>
-            {
-                ForceCompletion();
-                zavierzone.onZoneClear = null;
-            };
-            zavierzone.activateDeploy = true;
+            zone.remoteUpdater = RemoteUpdate;
         }
+        else
+        {
+            Debug.LogError("t un moron");
+            myOnComplete = onComplete;
+            ForceCompletion();
+        }
+    }
+
+    private void RemoteUpdate()
+    {
+        if (!CheckOrder())
+        {
+            //No order ! Quit
+            ForceCompletion();
+            return;
+        }
+
+        if (CheckCarriedPizza())
+        {
+            //Go to order
+
+            //Go to pizza
+            if (!goingToOrder)
+            {
+                hero.brain.GoToNode(currentOrderNode, Brain.Mode.pickup);
+            }
+        }
+        else
+        {
+            //Check available pizzas
+            if (!CheckPizzaNode())
+            {
+                //No pizza ! Quit
+                ForceCompletion();
+                return;
+            }
+
+            //Go to pizza
+            if (!goingToPizza)
+            {
+                hero.brain.GoToNode(currentPizzaNode, Brain.Mode.pickup);
+            }
+        }
+    }
+
+    private bool CheckOrder()
+    {
+        if (currentOrderNode != null && currentOrderNode.Order != null)
+            return true;
+
+        Node newOrderNode = zone.GetClosestNodeWithOrder();
+        
+        // Nouvelle order ?
+        if(newOrderNode != null)
+        {
+            currentOrderNode = newOrderNode;
+            return true;
+        }
+
+        goingToOrder = false;
+
+        return false;
+    }
+    private bool CheckCarriedPizza()
+    {
+        return hero.carriedPizza != null;
+    }
+    private bool CheckPizzaNode()
+    {
+        if (currentPizzaNode != null && currentPizzaNode.pizza != null)
+            return true;
+
+        Node newPizzaNode = zone.GetClosestNodeWithPizza();
+
+        // Nouvelle order ?
+        if (newPizzaNode != null)
+        {
+            currentPizzaNode = newPizzaNode;
+            return true;
+        }
+
+        goingToPizza = false;
+
+        return false;
     }
 
     public override HeroActions GetHeroActionInfo()
@@ -56,5 +144,10 @@ public class DeployAction : HeroActionEvent
     public override void PostCloneCleanup()
     {
         deployActionInfo = new DeployActionInfo();
+        zone = null;
+        myOnComplete = null;
+        hero = null;
+        currentOrderNode = null;
+        currentPizzaNode = null;
     }
 }
---
+++ GameJam_Unity/Assets/Game/Tests/Alex/ZavierZoneTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZavierZoneTrigger : MonoBehaviour {
+
+    public ZavierZone zone;
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        print("enter");
+
+        Node n = col.GetComponent<Node>();
+
+        if (n != null)
+        {
+            zone.nodes.Add(n);
+            return;
+        }
+    }
+    public void OnTriggerExit2D(Collider2D col)
+    {
+        print("exit");
+
+        Node n = col.GetComponent<Node>();
+
+        if (n != null)
+        {
+            zone.nodes.Remove(n);
+            return;
+        }
+    }
+}
