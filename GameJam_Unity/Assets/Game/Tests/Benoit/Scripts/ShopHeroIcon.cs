5a40943320dcebfb6a91e4babebb8058f2d3b983
Fred Bessette <msfredb7@gmail.com>
2017-10-07 20:10:47 -0400

    UI Tweaks

--- GameJam_Unity/Assets/Game/Tests/Benoit/Scripts/ShopHeroIcon.cs
+++ GameJam_Unity/Assets/Game/Tests/Benoit/Scripts/ShopHeroIcon.cs
@@ -3,22 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ShopHeroIcon : MonoBehaviour {
+public class ShopHeroIcon : MonoBehaviour
+{
 
-    
+
     public Hero heroPrefab;
     public float cost;
+    public HeroShop_Script parentPanel;
 
     private Hero heroInstance = null;
     private bool heroSelected = false;
-
-    public void clickDragHero()
-    {
-        heroSelected = true;
-        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        pos.z = 0;
-        heroInstance = Instantiate(heroPrefab, pos, Quaternion.identity);
-    }
 
     public void ReleaseDragHero()
     {
@@ -32,23 +26,19 @@ public class ShopHeroIcon : MonoBehaviour {
         Game.HeroManager.AddHero(heroInstance);
     }
 
-    //  methode qui gère le click de héro
-    //  1er click, prend un héro et change le curseur
-    //  2em click, lache le héro sur la map et remet le curseur
+    //  methode qui gï¿½re le click de hï¿½ro
+    //  1er click, prend un hï¿½ro et change le curseur
+    //  2em click, lache le hï¿½ro sur la map et remet le curseur
     public void ClickHero()
     {
-        print("dans le click");
-
         if (heroSelected == false)
         {
-            print("dans le false");
             heroSelected = true;
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             pos.z = 0;
             heroInstance = Instantiate(heroPrefab, pos, Quaternion.identity);
+            parentPanel.hideList();
         }
-
-        print("sortie de click");
     }
 
 
@@ -57,9 +47,18 @@ public class ShopHeroIcon : MonoBehaviour {
     {
         heroSelected = false;
         heroInstance.SnapToNode();
+        Game.HeroManager.AddHero(heroInstance);
+
+        //TODO: Decrease Cash
+    }
+    public void DestroyHero()
+    {
+        heroSelected = false;
+        if (heroInstance != null)
+            Destroy(heroInstance.gameObject);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         if (heroSelected && heroInstance != null)
@@ -69,17 +68,20 @@ public class ShopHeroIcon : MonoBehaviour {
 
             heroInstance.transform.position = pos;
 
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(1))
             {
-                print("dans le true");
-                if (EventSystem.current.IsPointerOverGameObject())
-                {
-                    Destroy(heroInstance);
-                    heroInstance = null;
-                    return;
-                }
+                //Cancel !
+                DestroyHero();
+            }
+            else if (Input.GetMouseButtonDown(0))
+            {
+                //Paste
+                //if (EventSystem.current.IsPointerOverGameObject())
+                //{
+                //    DestroyHero();
+                //    return;
+                //}
                 AddHeroToWorld();
-                Game.HeroManager.AddHero(heroInstance);
             }
         }
     }
--- GameJam_Unity/Assets/Game/Tests/Gab Simard/HireButton.cs
+++ GameJam_Unity/Assets/Game/Tests/Gab Simard/HireButton.cs
@@ -1,29 +1,13 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HireButton : MonoBehaviour {
 
     public HeroShop_Script ScriptHire;
-    public bool m_HireHide;
 
-    void Start()
-    {
-        m_HireHide = false;
-    }
-
-    //  Afficher ou cacher la liste des héros disponible a l'embauche
     public void toggleHire()
     {
-        if (m_HireHide == false)
-        {
-            m_HireHide = true;
-            ScriptHire.showList();
-        }
-        else
-        {
-            m_HireHide = false;
-            ScriptHire.hideList();
-        }
+        ScriptHire.ToggleVisibility();
     }
 }
